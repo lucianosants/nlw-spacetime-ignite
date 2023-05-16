e73ad3a3c82741015ee656f68c95beb9c186f9aa
lucianosants <lucianola18@gmail.com>
2023-05-16 08:59:56 -0300

    feat: aula 01 - frontend app

---
+++ front-end/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="h-screen bg-zinc-950 p-6 text-zinc-50">
+        <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+      </h1>
+    </div>
+  )
+}
